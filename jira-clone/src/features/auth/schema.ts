f26c7cc9223b55a0eb1495775eee3892429b5778
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-10-20 23:08:44 +0900

    Creating auth API 2:17:57(안됨)

--- jira-clone/src/features/auth/schema.ts
+++ jira-clone/src/features/auth/schema.ts
@@ -3,4 +3,5 @@ import { z } from "zod";
 export const loginSchema = z.object({
     email: z.string().email(),
     password: z.string().min(1, "Required"),
+    code: z.number(),
 });
--- jira-clone/src/features/auth/server/route.ts
+++ jira-clone/src/features/auth/server/route.ts
@@ -1,17 +1,17 @@
 import { Hono } from "hono";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
+import { loginSchema } from "../schema";
 const app = new Hono().post(
-    "/logIn",
-    zValidator(
-        "json",
-        z.object({
-            email: z.string().email(),
-            password: z.string(),
-        })
-    ),
+    "/login",
+    zValidator("json", loginSchema),
+    //zValidator("param", z.object({ userId: z.number()})),
     (c) => {
+        const { email, password } = c.req.valid("json");
+        console.log({ email, password });
+        //const {userId } = c.req.param();
         return c.json({ success: "ok" });
     }
 );
 export default app;
+//2:16:59
